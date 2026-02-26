21da89cb3691b6bf03043d122b64e5ce31b38f73
Arturo Roncal <roncalhermenegildo@gmail.com>
2026-02-26 14:25:06 -0500

    Initial commit

---
+++ src/main/java/com/arturo/releaseradarapi/ReleaseradarApiApplication.java
@@ -0,0 +1,13 @@
+package com.arturo.releaseradarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseradarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseradarApiApplication.class, args);
+    }
+
+}
