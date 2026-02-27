cb298bb3acaa7189f388f01371dda9070ed8e697
Arturo Roncal <roncalhermenegildo@gmail.com>
2026-02-27 02:48:31 -0500

    feat(db): agregar repositorios JPA

---
+++ src/main/java/com/arturo/releaseradarapi/repository/TituloRepository.java
@@ -0,0 +1,10 @@
+package com.arturo.releaseradarapi.repository;
+
+import com.arturo.releaseradarapi.entity.Titulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TituloRepository extends JpaRepository<Titulo, Long> {
+    Optional<Titulo> findByTmdbId(Integer tmdbId);
+}
