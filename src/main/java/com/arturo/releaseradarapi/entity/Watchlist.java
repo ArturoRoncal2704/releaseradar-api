5cba3ad60e6572eb83736fdbea5a460a7ed4dbab
Arturo Roncal <roncalhermenegildo@gmail.com>
2026-02-26 20:42:35 -0500

    fix(db): agregar configuraciones faltantes en las entidades

--- src/main/java/com/arturo/releaseradarapi/entity/Watchlist.java
+++ src/main/java/com/arturo/releaseradarapi/entity/Watchlist.java
@@ -3,6 +3,8 @@ package com.arturo.releaseradarapi.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "tb_watchlist", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"usuario_id","titulo_id"})
@@ -19,5 +21,25 @@ public class Watchlist {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "titutlo_id",nullable = false)
+    private Titulo titulo;
+
+    @Column(nullable = false)
+    private Boolean notificado;
+
+    @Column(name = "fecha_agregado",updatable = false)
+    private LocalDateTime fechaAgregado;
+
+    @PrePersist
+    protected  void OnCreate(){
+        this.fechaAgregado = LocalDateTime.now();
+                if(this.notificado == null){
+                    this.notificado = false;
+                }
+    }
 }
